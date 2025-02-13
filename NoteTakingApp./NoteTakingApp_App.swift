d471a7ae1c7bc4936ea420a84f757d651d43255a
Judah Lomo <judahllomo@gmail.com>
2025-02-12 21:03:53 -0600

    Initial Commit

---
+++ NoteTakingApp./NoteTakingApp_App.swift
@@ -0,0 +1,17 @@
+//
+//  NoteTakingApp_App.swift
+//  NoteTakingApp.
+//
+//  Created by Judah Lomo on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct NoteTakingApp_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NoteTakingApp.Tests/NoteTakingApp_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  NoteTakingApp_Tests.swift
+//  NoteTakingApp.Tests
+//
+//  Created by Judah Lomo on 2/12/25.
+//
+
+import Testing
+@testable import NoteTakingApp_
+
+struct NoteTakingApp_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
